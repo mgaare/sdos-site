897f0b6aa5c8bb49b389dbc54ab0d1785290572c
Michael Gaare <michael@clickscape.com>
2013-09-20 16:43:33 -0400

    Made user namespace a little more helpful with extra includes

--- dev/user.clj
+++ dev/user.clj
@@ -4,5 +4,11 @@
             [clojure.set :as set]
             [clojure.pprint :refer (pprint)]
             [clojure.test :as test]
+            [clj-time.core :refer (date-time)]
+            [clj-time.format :as tf]
+            [clj-time.coerce :as tc]
+            [korma.core :as k]
+            [korma.db :as db]
             [sdos-site.core :as core]
+            [sdos-site.article :as art]
             [clojure.tools.namespace.repl :refer (refresh refresh-all)]))
